750fd6836f0766f6ffac0a849dc04352393f1923
Rai Lee <thisisrailee@gmail.com>
2017-06-02 14:07:31 -0700

    rl-workfrom6_2

---
+++ src/components/logo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+class Logo extends Component {
+    render() {
+        return (
+            <div>
+                <img className="bee" src="https://thumb1.shutterstock.com/display_pic_with_logo/985355/338569370/stock-vector-honey-bee-carry-a-bucket-full-of-honey-vector-cartoon-338569370.jpg"/>
+            </div>
+        )
+    }
+}
+
+export default Logo; 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,12 @@ class Nav extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-default">
+            <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                    <a className="navbar-brand" href="#">
-                        <img alt="Brand" src=""/>
-                    </a>
+                        <a className="navbar-brand" href="#">
+                            <img alt="Brand" src=""/>
+                        </a>
                     </div>
                     <ul className="nav navbar-nav navbar-right">
                         <li><a href="#">Home</a></li>
