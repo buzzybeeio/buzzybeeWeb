3cc52c7daf1a7045b65883dcf1bff8c762081065
jason00111 <emberleyjason@gmail.com>
2017-07-08 11:59:00 -0700

    changes getJobs method to use promises

--- src/routes/Jobs.jsx
+++ src/routes/Jobs.jsx
@@ -4,26 +4,27 @@ import '../App.css'
 import $ from 'jquery'
 
 class Jobs extends Component {
-
     getJobs() {
-      const url = 'http://buzzybeeapi.herokuapp.com/'
+      const url = 'http://buzzybeeapi.herokuapp.co/'
 
-      $.ajax({
-        type:"GET",
-        url:url,
-        dataType:"json",
-        success: function(data){
-          console.log('data:', data)
-        },
-        error:  function(errorMessaging){
-          console.log('error:', errorMessaging)
-        }
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type:"GET",
+          url:url,
+          dataType:"json",
+          success: resolve,
+          error: reject
+        })
+
       })
+
 
     }
 
     render() {
         this.getJobs()
+        .then(console.log)
+        .catch((error) => console.log('the error:', error))
 
         return(
             <div>
