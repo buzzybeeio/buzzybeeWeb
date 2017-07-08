d25f3c40661be9d960391bb754efa2556cb90c84
jason00111 <emberleyjason@gmail.com>
2017-07-08 11:37:50 -0700

    Added ajax call to api

--- src/routes/Jobs.jsx
+++ src/routes/Jobs.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import Nav from '../components/navbar';
 import '../App.css'
+import $ from 'jquery'
 
 class Jobs extends Component {
 
+    getJobs() {
+      const url = 'http://buzzybeeapi.herokuapp.com/'
+
+      $.ajax({
+        type:"GET",
+        url:url,
+        dataType:"json",
+        success: function(data){
+          console.log('data:', data)
+        },
+        error:  function(errorMessaging){
+          console.log('error:', errorMessaging)
+        }
+      })
+
+    }
+
     render() {
+        this.getJobs()
+
         return(
             <div>
                 <div>
@@ -15,4 +35,4 @@ class Jobs extends Component {
     }
 }
 
-export default Jobs; 
+export default Jobs;
