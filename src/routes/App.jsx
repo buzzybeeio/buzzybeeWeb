84c623ed2003cae241aab50e1aebcb391ea38630
Hernan Mendez <hernan21112001@gmail.com>
2017-07-24 16:58:44 -0400

    added intro animation

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,13 +4,18 @@ import Logo from '../components/logo';
 import Textbox from '../components/textbox';
 import Mission from '../components/mission';
 import Footer from '../components/Footer';
+import $ from 'jquery';
 import '../App.css'
 
 class App extends Component {
 
     render() {
         return(
-            <div className="logo-container">
+            <div>
+                <div className='animation'>
+                    <div className='cube'></div>
+                </div>
+                <div className="logo-container">
                 <div>
                     <Nav />
                 </div>
@@ -25,9 +30,19 @@ class App extends Component {
                 <div className="container mission_container">
                     <Mission />
                 </div>
-                <Footer />
+                    <Footer />
+                </div>
             </div>
+            
         )
+    }
+
+    componentDidMount(){
+        $('.logo-container').css('display', 'none')
+        setTimeout(function(){
+            $('.animation').css('display', 'none')
+            $('.logo-container').fadeIn(600)
+        },2000)
     }
 }
 
