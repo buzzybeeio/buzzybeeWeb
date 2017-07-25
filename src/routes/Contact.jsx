49b26ab2198aa3485717c031c97629b88c385e28
Hernan Mendez <hernan21112001@gmail.com>
2017-07-25 18:12:25 -0400

    Navbar moved to outside the Switch in for non-repetition purposes

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Nav from '../components/navbar';
 import ContactForm from '../components/ContactForm';
-import Footer from '../components/Footer';
-import Logo from '../components/logo'
-import '../App.css'
+import Logo from '../components/logo';
+import '../App.css';
 
 class Contact extends Component {
     render() {
-        return(
+        return (
             <div>
-                <div>
-                    <Nav />
-                </div>
                 <Logo />
                 <ContactForm />
-                <Footer />
             </div>
         )
     }
