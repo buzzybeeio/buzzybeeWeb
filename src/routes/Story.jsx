c6a051e1cc095b0056d7de19a915b78cc1bd7ca8
Rai Lee <thisisrailee@gmail.com>
2017-08-22 22:24:28 -0700

    Fix Footer Bug

--- src/routes/Story.jsx
+++ src/routes/Story.jsx
@@ -33,7 +33,7 @@ class Story extends Component {
                         {
                             this.state.stories.map(info => {
                                 var infostr = JSON.stringify(info)
-                                return (<div className="story-box" data={infostr}><img src={'profilepics/' + info.name + '.jpg'} alt={info.name + ' img'} /><span>{info.name}</span></div>)
+                                return (<div className="story-box" key={infostr} data={infostr}><img src={'profilepics/' + info.name + '.jpg'} alt={info.name + ' img'} /><span>{info.name}</span></div>)
                             })
                         }
                     </div>
