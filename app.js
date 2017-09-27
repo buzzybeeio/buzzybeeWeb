3237f00d685c2962536639af02de2b153a2d337f
Rai Lee <thisisrailee@gmail.com>
2017-09-27 10:41:07 -0700

    force ssl

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const mailRouter = require('./contact/mail');
 
 const port = process.env.PORT || 3000;
 
+let forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+ };
+
+app.use(forceSsl);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 app.use(mailRouter);
-
-app.get('/.well-known/acme-challenge/t6d4ttYOi770nZn4fWNnP_4VqBzOPm0PPKTcCjv2IgU', (req, res) => {
-  res.send('t6d4ttYOi770nZn4fWNnP_4VqBzOPm0PPKTcCjv2IgU.Sh6Vb9MeILvkeyM3wlDw2OkFFGTY04I7DX6HoBGb2ss')
-})
 
 app.get(/.*\/$/, (req, res) => {
   res.redirect(req.originalUrl.slice(0, -1));
